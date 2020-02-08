aaf08a2047b528af6cf0c983da71e1f360d00f57
MiddleFinger1 <45088113+MiddleFinger1@users.noreply.github.com>
2020-02-08 19:16:09 +0400

    updateX.2.1.1

--- app/src/main/java/com/logic/IOManager.kt
+++ app/src/main/java/com/logic/IOManager.kt
@@ -18,19 +18,55 @@ object IOManager {
     const val appDirectory = "Questionnaire"
 
     fun downloadUser(context: Context) {
-        val json = readFile(userFileName)
-        Log.e("jsonDownloadUser", json)
-        val user = if (json != "")
+        val json =
+            try {
+                readFile(userFileName)
+            }
+            catch(ex: Exception){
+                Log.e("exReadFile", ex.toString())
+                null
+            }
+        Log.e("jsonDownloadUser", json.toString())
+        val user = if (json != null && json.toString().isNotEmpty())
             User.createUser(json)
         else createUser(context)
         if (user != null)
             IOManager.user = user
     }
 
+    private fun findFolder(){
+
+    }
+
     private fun createUser(context: Context): User? {
+        /*
+        File fileName = null;
+String sdState = android.os.Environment.getExternalStorageState();
+if (sdState.equals(android.os.Environment.MEDIA_MOUNTED)) {
+File sdDir = android.os.Environment.getExternalStorageDirectory();
+fileName = new File(sdDir, "cache/primer.txt");
+} else {
+fileName = context.getCacheDir();
+}
+if (!fileName.exists())
+fileName.mkdirs();
+try {
+FileWriter f = new FileWriter(fileName);
+f.write("hello world");
+f.flush();
+f.close();
+} catch (Exception e) {
+
+}
+         */
+
         val folder = createFolder(appDirectory)
-            File(folder, userFileName)
-            File(folder, constructorFileName)
+        val fileUser = File(folder, userFileName)
+        val fileConst = File(folder, constructorFileName)
+
+        FileWriter(fileUser)
+        FileWriter(fileConst)
+
         val json = Helper.converting(context.assets.open(userFileName))
         writeFile(userFileName, json)
         return User.createUser(json)
